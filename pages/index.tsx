e404b45ac5fc20cca5df5cc583bcc4e34d097fc2
Yeo Swe Hou <swehou@gmail.com>
2022-04-30 01:45:12 +0900

    default bg color

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className='text-red-200 bg-pink-50'>Gekllo</div>
+    <div className='text-red-200'>Gekllo</div>
   )
 }
 
