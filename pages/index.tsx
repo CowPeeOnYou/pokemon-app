e5b705c3463909ea749596050a55491484378e5d
Yeo Swe Hou <swehou@gmail.com>
2022-04-30 01:55:37 +0900

    basic layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
-import type { NextPage } from 'next'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className='text-red-200'>Gekllo</div>
-  )
-}
+    <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="text-2xl text-center">Which Pokemon is cuter?</div>
+      <div className="p-2"/>
+      <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
+        <div className="w-16 h-16 bg-red-200" />
+        <div className="p-8">Vs</div>
+        <div className="w-16 h-16 bg-red-200" />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
