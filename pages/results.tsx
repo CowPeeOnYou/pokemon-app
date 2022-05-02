b23b987a760c68835acc5ec4c17a1b4f0c052abe
Yeo Swe Hou <swehou@gmail.com>
2022-05-03 01:36:08 +0900

    result page!

--- pages/results.tsx
+++ pages/results.tsx
@@ -3,6 +3,7 @@ import { count } from "console";
 import type { GetServerSideProps } from "next";
 import { prisma } from "../backend/utils/prisma";
 import { AsyncReturnType } from "../utils/ts-bs";
+import Image from "next/image";
 
 const getPokemonInOrder = async () => {
   return await prisma.pokemon.findMany({
@@ -23,15 +24,44 @@ const getPokemonInOrder = async () => {
   });
 };
 
+type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
+
+const PokemonList: React.FC<{ pokemon: PokemonQueryResult[number] }> = (
+  props
+) => {
+  return (
+    <div className="flex border-b p-2 items-center w-full max-w-2xl">
+      {" "}
+      <Image
+        layout="fixed"
+        src={props.pokemon.spriteUrl}
+        width={64}
+        height={64}
+      />
+      <div className="capitalize">{props.pokemon.name}</div>
+    </div>
+  );
+};
+
 const ResultsPage: React.FC<{
   pokemon: AsyncReturnType<typeof getPokemonInOrder>;
 }> = (props) => {
-  return <div>Enter</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-2xl p-4">Results</h1>
+      <div className="flex flex-col items-center w-full max-w-2xl border">
+        <div className="p-2"/.
+      {props.pokemon.map((currentPokemon, index) => {
+        return <PokemonList pokemon={currentPokemon} key={index} />;
+      })}
+    </div>
+    </div>
+  );
 };
 
 export default ResultsPage;
 
 export const getStaticProps: GetServerSideProps = async () => {
   const orderedPokemon = await getPokemonInOrder();
-  return { props: { pokemon: orderedPokemon },revalidate: 60};
+  return { props: { pokemon: orderedPokemon }, revalidate: 60 };
 };
