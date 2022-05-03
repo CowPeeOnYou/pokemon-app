964510c4f0393245f60eeaa7482e1680ae39df24
Yeo Swe Hou <swehou@gmail.com>
2022-05-03 18:55:26 +0900

    trtied other settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,20 +8,50 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
+// export default withTRPC<AppRouter>({
+//   config({ ctx }) {
+//     /**
+//      * If you want to use SSR, you need to use the server's full URL
+//      * @link https://trpc.io/docs/ssr
+//      */
+//     const url = process.env.VERCEL_URL
+//       ? `https://${process.env.VERCEL_URL}/api/trpc`
+//       : 'http://localhost:3000/api/trpc';
+
+//     return {
+//       url,
+//       /**
+//       //  * @link https://react-query.tanstack.com/reference/QueryClient
+//        */
+//       // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+//     };
+//   },
+//   /**
+//    * @link https://trpc.io/docs/ssr
+//    */
+//   ssr: false,
+// })(MyApp);
+
+
+function getBaseUrl() {
+  if (process.browser) return ""; // Browser should use current path
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+}
+
 export default withTRPC<AppRouter>({
   config({ ctx }) {
     /**
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = `${getBaseUrl()}/api/trpc`;
 
     return {
       url,
       /**
-      //  * @link https://react-query.tanstack.com/reference/QueryClient
+       * @link https://react-query.tanstack.com/reference/QueryClient
        */
       // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
     };
@@ -29,5 +59,5 @@ export default withTRPC<AppRouter>({
   /**
    * @link https://trpc.io/docs/ssr
    */
-  ssr: true,
+  ssr: false,
 })(MyApp);
